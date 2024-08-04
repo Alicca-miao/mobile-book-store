02cce2f790329e7497523cff45caef4b750c6836
Alicca-miao <yusangmuran@163.com>
2024-08-04 15:26:58 +0800

    前端 Ts+Pinia+uni-app+Vue3

---
+++ mobilebookstore_frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    // 开发阶段启用源码映射：https://uniapp.dcloud.net.cn/tutorial/migration-to-vue3.html#需主动开启-sourcemap
+    sourcemap: process.env.NODE_ENV === 'development',
+  },
+  plugins: [uni()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 配置 less-loader 选项
+        javascriptEnabled: true
+      }
+    }
+  },
+  server: {
+    proxy: {
+      // 将请求代理到目标服务器
+      '/api': {
+        target: 'http://localhost:3000', // 目标服务器地址
+        changeOrigin: true, // 是否改变请求源
+        rewrite: (path) => path.replace(/^\/api/, ''), // 重写路径
+      },
+    },
+  },
+})
